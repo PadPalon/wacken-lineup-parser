3754ff6f52ab526b3072b96e92910260a9eb1839
Luca Neukom <luca.neukom@gmail.com>
2022-08-09 19:17:14 +0200

    create initial version

---
+++ parser.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+
+def read_html():
+    with open("input.html", "r", encoding = "utf-8") as html:
+        return BeautifulSoup(html.read(), features="html.parser")
+
+
+html = read_html()
+elements = html.select("h5.card-title")
+with open("output", "w", encoding = "utf-8") as output:
+    output.truncate(0)
+    output.writelines("%s\n" % element.text for element in elements)
